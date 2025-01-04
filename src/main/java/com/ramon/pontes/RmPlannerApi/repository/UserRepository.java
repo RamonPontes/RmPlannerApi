6e9e84dfb9a69314f76397231e8c1bc7dfde7e75
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 20:33:57 -0300

    feat: edit user repository for spring security

--- src/main/java/com/ramon/pontes/RmPlannerApi/repository/UserRepository.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.ramon.pontes.RmPlannerApi.repository;
 
 import com.ramon.pontes.RmPlannerApi.model.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    UserDetails findByEmail(String email);
 }
