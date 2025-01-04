33739504abe590865d341c4d0292d15fb9531270
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 20:58:27 -0300

    feat: authorizeHttpRequests in SecurityConfiguration

--- src/main/java/com/ramon/pontes/RmPlannerApi/infra/security/SecurityConfiguration.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.ramon.pontes.RmPlannerApi.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,8 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
                 .build();
     }
 }
