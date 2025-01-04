0673a5733c9dd19891c1bdfad41c86d64b2fbd95
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 19:40:41 -0300

    git init

---
+++ src/main/java/com/ramon/pontes/RmPlannerApi/RmPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.pontes.RmPlannerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramon/pontes/RmPlannerApi/RmPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.pontes.RmPlannerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
