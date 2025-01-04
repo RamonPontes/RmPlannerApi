6f22726b32dafc386b9ff2971d3027a0321bc400
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 20:15:44 -0300

    feat: add password in user model

--- src/main/java/com/ramon/pontes/RmPlannerApi/model/User.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/model/User.java
@@ -11,6 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
+    private String password;
     private String email;
     private String account_type;
 
@@ -28,6 +29,14 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = username;
     }
 
     public String getEmail() {
