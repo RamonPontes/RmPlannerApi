96fec896e5f572731aa4071770476dd813200cf0
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 20:16:26 -0300

    feat: add db.sql

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    account_type VARCHAR(50) NOT NULL
+)
