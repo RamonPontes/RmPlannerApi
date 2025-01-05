8d85178b6f9513a4394c961ebb207b067ee4d927
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 21:21:18 -0300

    feat: create /register router

--- src/main/java/com/ramon/pontes/RmPlannerApi/Controller/AuthController.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/Controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.ramon.pontes.RmPlannerApi.Controller;
 
 import com.ramon.pontes.RmPlannerApi.dto.AuthDTO;
+import com.ramon.pontes.RmPlannerApi.dto.RegisterDTO;
+import com.ramon.pontes.RmPlannerApi.model.User.User;
+import com.ramon.pontes.RmPlannerApi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +17,25 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/login")
-    public ResponseEntity<?> authLogin(@RequestBody AuthDTO data){
+    public ResponseEntity<?> authLogin(@RequestBody AuthDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> authRegister(@RequestBody RegisterDTO data) {
+        if (this.userRepository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.username(), data.email(), encryptedPassword, data.account_type());
+
+        this.userRepository.save(newUser);
         return ResponseEntity.ok().build();
     }
 }
