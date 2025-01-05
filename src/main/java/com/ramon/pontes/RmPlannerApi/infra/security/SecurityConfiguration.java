30ff5545abe3e6b91d800eb50e53c7dcb637d5f0
RamonPontes <rgdpontes@yahoo.com>
2025-01-04 21:05:22 -0300

    feat: add AuthenticationManager in SecurityConfiguration

--- src/main/java/com/ramon/pontes/RmPlannerApi/infra/security/SecurityConfiguration.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/infra/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.ramon.pontes.RmPlannerApi.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,5 +22,10 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
