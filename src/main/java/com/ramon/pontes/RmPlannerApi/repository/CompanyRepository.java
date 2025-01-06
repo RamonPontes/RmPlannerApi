a0e908c958783761f25516f90da2f3d7be8d9b75
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 12:55:31 -0300

    feat: add findAllByOwner in CompanyRepository

--- src/main/java/com/ramon/pontes/RmPlannerApi/repository/CompanyRepository.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/repository/CompanyRepository.java
@@ -3,5 +3,8 @@ package com.ramon.pontes.RmPlannerApi.repository;
 import com.ramon.pontes.RmPlannerApi.model.Company.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+    List<Company> findAllByOwner(int owner);
 }
