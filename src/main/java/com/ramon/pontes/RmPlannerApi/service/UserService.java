d563d048b31f4bb18d81d2bf1fd8fa370e9d5ace
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 12:22:25 -0300

    feat: create user service

---
+++ src/main/java/com/ramon/pontes/RmPlannerApi/service/UserService.java
@@ -0,0 +1,19 @@
+package com.ramon.pontes.RmPlannerApi.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String getAuthenticatedUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return userDetails.getUsername();
+        }
+
+        return null;
+    }
+}
