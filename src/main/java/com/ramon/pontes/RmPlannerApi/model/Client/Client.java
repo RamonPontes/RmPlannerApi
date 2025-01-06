d4d590efdcc8351b679c7c04f040d84f1df69108
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 19:28:25 -0300

    fix: fix client model

--- src/main/java/com/ramon/pontes/RmPlannerApi/model/Client/Client.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/model/Client/Client.java
@@ -1,6 +1,14 @@
 package com.ramon.pontes.RmPlannerApi.model.Client;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String phone;
