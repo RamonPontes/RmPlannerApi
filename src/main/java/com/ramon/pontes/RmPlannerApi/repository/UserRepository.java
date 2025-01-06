3a09defa3235c13bf6ab5513a42e032afe73b5ad
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 12:22:40 -0300

    feat: add findUserByEmail to return User model

--- src/main/java/com/ramon/pontes/RmPlannerApi/repository/UserRepository.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     UserDetails findByEmail(String email);
+    User findUserByEmail(String email);
 }
