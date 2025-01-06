7177ec73cc5e42b3470783c8e3314345f458dec2
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 12:55:53 -0300

    feat: create getAllUserCompany in CompanyService

--- src/main/java/com/ramon/pontes/RmPlannerApi/service/CompanyService.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/service/CompanyService.java
@@ -6,6 +6,8 @@ import com.ramon.pontes.RmPlannerApi.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CompanyService {
     @Autowired
@@ -13,5 +15,9 @@ public class CompanyService {
 
     public void CreateCompany(Company data) {
         companyRepository.save(data);
+    }
+
+    public List<Company> getAllUserCompany(int owner_id) {
+        return companyRepository.findAllByOwner(owner_id);
     }
 }
