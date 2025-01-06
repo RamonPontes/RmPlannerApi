48079b645233383a4838a87c748c4c2d14b819e4
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 19:48:56 -0300

    feat: remove client router and add schedule in db.sql

--- src/main/java/com/ramon/pontes/RmPlannerApi/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ramon.pontes.RmPlannerApi.repository;
-
-import com.ramon.pontes.RmPlannerApi.model.Client.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
