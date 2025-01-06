5d7d6c07a1b03047a1d7019e07b7d70b015a45a0
RamonPontes <rgdpontes@yahoo.com>
2025-01-06 00:23:00 -0300

    feat: create Company repository

--- src/main/java/com/ramon/pontes/RmPlannerApi/repository/CompanyRepository.java
+++ src/main/java/com/ramon/pontes/RmPlannerApi/repository/CompanyRepository.java
@@ -1,4 +1,7 @@
 package com.ramon.pontes.RmPlannerApi.repository;
 
-public class CompanyRepository {
+import com.ramon.pontes.RmPlannerApi.model.Company.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 }
